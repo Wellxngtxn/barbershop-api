2aacbd6921375be3f1a26d2048d8b5ea1a7ee032
Wellxngton <wellingtoncerqueira17@gmail.com>
2026-05-08 19:43:23 -0300

    Initial Spring Boot project setup

---
+++ src/test/java/github/Wellxngton/barbershop_api/BarbershopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package github.Wellxngton.barbershop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbershopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
